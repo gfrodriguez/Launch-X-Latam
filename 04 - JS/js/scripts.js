d097b85e9daa68e53bd85642526956a74e74c1d1
gfrodriguez <rggf2042143@gmail.com>
2022-11-06 23:30:03 -0500

    pokéDex
    Se envían ajustes al pokéDex, permite buscar una imagen HD, descripción del pokemón en español, altura y peso

--- 04 - JS/js/scripts.js
+++ 04 - JS/js/scripts.js
@@ -1,28 +1,46 @@
 const buttonPokeSearch = document.getElementById("buttonPokeSearch");
 const alertNull = document.getElementById("alertNull")
-const alertThatPokeDoesNotExist = document.getElementById("alertThatPokeDoesNotExist")
+const alertThatPokeDoesNotExist = document.getElementById("alertThatPokeDoesNotExist");
+const pokeInfo=[];
 //const pokeSearch = document.getElementById("pokeSearch");
-//const pokeName = document.getElementById("pokeName");
+//const pokeDescription = document.getElementById("pokeDescription");
 
 const callPokeAPI = async () => {
-  if(!pokeSearch.value==""){
-    alertNull.classList.add("d-none");
-    try {
-      const res = await fetch("https://pokeapi.co/api/v2/pokemon/"+pokeSearch.value);
-      alertThatPokeDoesNotExist.classList.add("d-none");
-      const data = await res.json();
-      pokeName.innerText = data.name;
-      pokeImage.src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/"+(data.id > 99 ? data.id : "0"+data.id)+".png";
-      console.log(data)
-    } catch (e) {
-      alertThatPokeDoesNotExist.classList.remove("d-none");
-      pokeImage.src="images/nofound.gif"
-    }
-  }else{
-    alertNull.classList.remove("d-none");
-    pokeImage.src="images/null.png"
+  if (!pokeSearch.value == "") {
+      pokeSearch.value = pokeSearch.value.toLowerCase();
+      alertNull.classList.add("d-none");
+      try {
+          const resPoke = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokeSearch.value);
+          const resPokeEspecies = await fetch("https://pokeapi.co/api/v2/pokemon-species/" + pokeSearch.value);
+          alertThatPokeDoesNotExist.classList.add("d-none");
+          const dataPoke = await resPoke.json();
+          const dataPokeEspecies = await resPokeEspecies.json();
+          pokeInfo.id = dataPoke.id;
+          pokeInfo.name = dataPoke.name;
+          pokeInfo.image = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + (pokeInfo.id > 99 ? pokeInfo.id  : pokeInfo.id > 10 ? "0" + pokeInfo.id : "00" + pokeInfo.id) + ".png";
+          pokeInfo.height = dataPoke.height*10;
+          pokeInfo.weight = dataPoke.weight/10;
+          for (let i of dataPokeEspecies.flavor_text_entries) {
+              if (i.language.name == 'es') {
+                  pokeInfo.description = i.flavor_text.replace(/\n/g, ' ');
+                  break
+              }
+          }
+          pokeDescription.innerText = pokeInfo.description;
+          pokeName.innerText = pokeInfo.name;
+          pokeImage.src = pokeInfo.image;
+          pokeId.innerText = "No. "+pokeInfo.id;
+          pokeHeight.innerText = pokeInfo.height+' cm';
+          pokeWeight.innerText = pokeInfo.weight+' Kg';
+          console.log(dataPoke);
+      } catch (e) {
+          alertThatPokeDoesNotExist.classList.remove("d-none");
+          pokeImage.src = "images/nofound.gif"
+      }
+  } else {
+      alertNull.classList.remove("d-none");
+      pokeImage.src = "images/null.png"
   }
-  
-};
+}
 
 buttonPokeSearch.addEventListener("click", callPokeAPI);
--- README.md
+++ README.md
@@ -4,13 +4,16 @@ En este repositorio se dejarán las asignaciones del curso a medida que se vayan
 
 ### [1. Practica de Intro a Frontend - Caso Abogabot](01%20-%20INTRO/ "1. Practica de Intro a Frontend - Caso Abogabot")
 
-### [2. Practica de HTML a Frontend - Caso Pastelería](02%20-%20HTML/ "2. Practica de HTML a Frontend - Caso Pastelería")
+### [2. Practica de HTML a Frontend - Caso Pastelería](02%20-%20HTML/ "2. Practica de HTML Frontend - Caso Pastelería")
+
+### [4. Practica de HTML a Frontend - Caso Pastelería](04%20-%20JS/ "4. Practica de JS Frontend - Caso Pokédex")
 
 ------------
 
 También puede ver el contenido desplegado en los siguientes enlaces:
 
 ### [Práctica HTML](https://launch-x-latam-frontend.herokuapp.com/02%20-%20HTML/ "Práctica HTML")
+### [Práctica JS](https://launch-x-latam-frontend.herokuapp.com/04%20-%20JS/ "Práctica JS")
 
 
 
